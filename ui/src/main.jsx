38cdcc555ab6890f9fd8856817d07dd8b203469e
Josh Freeman <josh@viion.co.uk>
2026-08-06 22:07:36 +0100

    Point README at the new public repository.
    This project has been renamed and relocated; the tree is cleared and the
    README redirects to the new home.

--- ui/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import '../css/theme.css';
-import '../css/app.css';
-import App from './App.jsx';
-
-createRoot(document.getElementById('root')).render(<App />);
